560ba79fdbf4ca5800dc1119af5787c7b7aeb799
hoimonti70 <hoimontirajbonsi@gmail.com>
2024-08-26 01:22:24 +0600

    Update schema.sql

--- server/utils/schema.sql
+++ server/utils/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT NOT NULL,
     user_id INT NOT NULL,
-    comment TEXT,
+    text TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (post_id) REFERENCES posts(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
